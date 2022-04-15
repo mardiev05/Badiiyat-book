68edb770e3c58047efb4834dbaa3ebea05e41fb6
mardiev.dev <mardiev2125@gmail.com>
2022-04-15 11:41:02 -0500

    folder has been changed

---
+++ Sign-up/signup.js
@@ -0,0 +1,55 @@
+let form = document.querySelector("#form");
+let url = "https://bookzon.herokuapp.com/api/";
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    if (e.target[4].value !== e.target[5].value) {
+        // Swal.fire({
+        //     icon: "error",
+        //     title: "Oops...",
+        //     text: "Something went wrong!",
+        //     footer: '<a href="">Why do I have this issue?</a>',
+        // });
+        let error = document.getElementById("password__msg");
+        error.innerHTML = "Password does not match";
+        error.style.color = "red";
+        return;
+    }
+    let user = {
+        email: e.target[2].value,
+        password: e.target.password.value,
+        firstName: e.target[0].value,
+        lastName: e.target[1].value,
+        phone: e.target[3].value,
+        // address: e.target[4].value,
+    };
+
+    (async() => {
+        let response = await fetch(url + "sign-up", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+        let data = await response.json();
+        if (data.success) {
+            localStorage.setItem("token", data.token);
+            Swal.fire({
+                icon: "success",
+                title: "Success",
+                text: "You have successfully registered",
+                footer: '<a href="">Why do I have this issue?</a>',
+            });
+            // window.location.href = "login.html";
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: data.msg,
+                footer: '<a href="">Why do I have this issue?</a>',
+            });
+        }
+        console.log(data);
+    })();
+});
