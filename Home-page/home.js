c3c3c9846c8da0f8e9f41db6e084e0961015cda8
mardiev.dev <mardiev2125@gmail.com>
2022-04-21 09:26:38 -0500

    home js has been completed

--- Home-page/home.js
+++ Home-page/home.js
@@ -4,5 +4,27 @@ let url = "http://localhost:8000/api";
 (async() => {
     let response = await fetch(url + "/authors");
     let data = await response.json();
-    console.log(data);
+    console.log(data.payload);
+    for (let item of data.payload) {
+        let card = `
+             <div class="card">
+                    <img src="../images/avloniy.png" alt="">
+                    <div class="card_content">
+                        <p class="card__text">
+                            ${item.firstName +" "+ item.lastName}<br>
+                            <span>${new Date(item.date_of_birth).getFullYear()} - ${new Date(item.date_of_death).getFullYear()}</span>
+                        </p>
+                        <div class="icons">
+                            <div class="folder">
+                                <i class="fa-solid fa-folder-open"></i>77
+                            </div>
+                            <div class="headphone">
+                                <i class="fa-solid fa-headphones-simple"></i>77
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        document.querySelector(".row").innerHTML += card
+    }
 })();
--- add-authors/authors.js
+++ add-authors/authors.js
@@ -1,16 +1,6 @@
 // const url = "https://bookzon.herokuapp.com/api";
 const url = "http://localhost:8000/api";
 
-let author = {
-    firstName: "William",
-    lastName: "Shekspare",
-    date_of_birth: "2021-07-13T14:23:25.696Z",
-    date_of_death: "2021-07-13T14:23:25.696Z",
-    createdAt: "2021-07-13T14:23:25.696Z",
-    updatedAt: "2021-07-13T14:23:25.696Z",
-    user: "userId",
-};
-
 let form = document.querySelector("#form");
 let user = JSON.parse(localStorage.getItem("user"));
 console.log(user);
@@ -24,7 +14,6 @@ form.addEventListener("submit", (e) => {
         date_of_death: e.target.date_of_death.value,
         // user: user._id,
     };
-
     (async() => {
         let res = await fetch(url + "/authors", {
             method: "POST",
